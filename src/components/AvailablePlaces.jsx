afc54002fcd54f190dcb0adaa140b519265acafb
jaypee1414 <jhnplbncyrn03@gmail.com>
2024-08-09 15:51:13 +0800

    add fetch data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
-
+import axios from 'axios';
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePalces] = useState([])
+
+  // fetch('http://localhost:3000/places').then((res)=>{
+  //   return res.json()
+  // }).then((data)=>{
+  //   setAvailablePalces(data.places)
+  // })
+
+
+  useEffect (()=>{
+    const fecthData = async () =>{
+      const data = await axios.get('http://localhost:3000/places')
+      setAvailablePalces(data?.data)
+  }
+  fecthData();
+  },[])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
